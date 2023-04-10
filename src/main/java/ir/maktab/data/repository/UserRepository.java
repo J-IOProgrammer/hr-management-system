e1144c452e1372868a514432793a53e096a5d596
J-IOProgrammer <fg.bonny@outlook.com>
2023-04-11 01:03:30 +0430

    edit repo part

--- src/main/java/ir/maktab/data/repository/UserRepository.java
+++ src/main/java/ir/maktab/data/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package ir.maktab.data.repository;
 
 import ir.maktab.data.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<Person, Long> {
+@Repository
+public interface UserRepository extends CrudRepository<Person, Long> {
 
     Optional<Person> findPersonByUsername(String username);
 
